b697b22f1e3e0e7005845208c713275709f9d319
Somn <somnacin7@gmail.com>
2017-04-08 19:23:00 -0400

    add events on trigger stuff

---
+++ Assets/Scripts/VRHand.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class VRHand : MonoBehaviour {
+
+    ulong grabInput;
+    SteamVR_TrackedObject controller;
+
+    public delegate void ButtonDown();
+    public event ButtonDown OnButtonDown;
+
+    public delegate void Button();
+    public event Button OnButton;
+
+    public delegate void ButtonUp();
+    public event ButtonUp OnButtonUp;
+
+    void Awake () {
+        controller = GetComponent<SteamVR_TrackedObject>();
+        grabInput = SteamVR_Controller.ButtonMask.Trigger;
+	}
+	
+	void Update () {
+        var device = SteamVR_Controller.Input((int)controller.index);
+        if (device.GetTouchDown(grabInput)) {
+            if (OnButtonDown != null) {
+                OnButtonDown();
+            }
+            Debug.Log("BasdfasdD");
+
+        }
+
+        if (device.GetTouch(grabInput) && OnButton != null)
+        {
+            OnButton();
+        }
+
+        if (device.GetTouchUp(grabInput) && OnButtonUp != null)
+        {
+            OnButtonUp();
+        }
+    }
+
+    private void OnEnable()
+    {
+        OnButtonDown += BD;
+        OnButton += B;
+        OnButtonUp += BU;
+    }
+
+    private void OnDisable()
+    {
+        OnButtonDown -= BD;
+        OnButton -= B;
+        OnButtonUp -= BU;
+    }
+
+    void BD()
+    {
+        Debug.Log("BD");
+    }
+
+    void B()
+    {
+        Debug.Log("B");
+    }
+
+    void BU()
+    {
+        Debug.Log("BU");
+    }
+}
