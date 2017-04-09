cb3a4d71f29228cf771b422cc1cd112a47000a77
Somn <somnacin7@gmail.com>
2017-04-09 10:57:22 -0400

    switch out music

--- Assets/Scripts/OsuManager.cs
+++ Assets/Scripts/OsuManager.cs
@@ -12,11 +12,8 @@ public class OsuManager : MonoBehaviour {
     /// </summary>
     public Transform beatSpawnPoint;
 
-    private string BEAT =
-@"X00000X0X0000000X0X000X0X0000000
-0000X0000000X0000000X0000000X000";
-    private char[] leftBeats;
-    private char[] rightBeats;
+    private string BEAT = @"0000 0000 0X00 0X00 0X0X 0X0X 0X0X 00X0 00X0 00Y- -0X0  00Y- -X0X 0X0Y --Y- ---X 0X00 00X0 0X0X 00Y- -X0X 00X0 00XX 0X0X 0000 0000 0000 00XX 0Y-- -0XX 0X0X 00XX 00X0 00Y- -0Y- -0X0 Y--- Y--- Y--- 00XX 0X0X 0Y-- -X0X 0Y-- X00X X00X 0Y-- -0XX 0Y-- X0X0 Y--X 0X0X 0XY- -0X0 0XY- -X0X 0XY- -XY- -X0X 0X0X X0X0 00XX Y--- -000";
+    private char[] beats;
     private int currentBeat = 0;
     private int currentPlayedBeat = -2  ;
 
@@ -25,9 +22,7 @@ public class OsuManager : MonoBehaviour {
 
     void Initialize()
     {
-        var beatLines = BEAT.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
-        leftBeats = beatLines[0].ToCharArray();
-        rightBeats = beatLines[1].ToCharArray();
+        beats = BEAT.Replace(" ", String.Empty).ToCharArray();
 
         var beat = beatPrefab.GetComponent<Beat>();
         var clipLength = 3.0f;
@@ -45,27 +40,23 @@ public class OsuManager : MonoBehaviour {
     private void Awake()
     {
         InvokeRepeating("PlayBeat", 0, 60.0f / bpm / 4.0f);
-        Invoke("StartBeat", 60.0f / bpm);
+        audioSource.PlayDelayed(60.0f / bpm);
     }
 
     void PlayBeat()
     {
+        var curBeatIndex = (currentBeat + 2) % beats.Length;
 
-        if (leftBeats[(currentBeat + 2) % leftBeats.Length] == 'X' || rightBeats[(currentBeat + 2) % leftBeats.Length] == 'X') {
+        if (beats[curBeatIndex] == 'X' || beats[curBeatIndex] == 'Y') {
             var spawnPos = beatPattern.GetBeatPosition(currentPlayedBeat);
             Instantiate(beatPrefab, spawnPos, Quaternion.identity);
             currentPlayedBeat++;
         }
 
         currentBeat++;
-        if (currentBeat >= leftBeats.Length)
+        if (currentBeat >= beats.Length)
         {
             currentBeat = 0;
         }
-    }
-
-    void StartBeat()
-    {
-        audioSource.Play();
     }
 }
